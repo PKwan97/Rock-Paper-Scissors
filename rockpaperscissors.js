fdda2785e4a7089c856ddfbffca3a4f6881afddc
Philip <philiprkwan@gmail.com>
2022-04-13 17:40:08 -0600

    Add computerPlay function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,28 @@
-console.log("test")
+function computerPlay(){
+let numberGen = Math.floor(Math.random()*3);
+console.log(numberGen);
+let computerMove = '';
+
+if (numberGen == 0){
+computerMove = "rock";
+}
+
+else if (numberGen == 1){
+computerMove = "paper";
+}
+
+else if (numberGen == 2){
+computerMove = "scissors";
+}
+console.log(computerMove);
+return computerMove;
+}
+
+function RPS(playerSelection,computerSelection){
+    if (playerSelection.toLowerCase !== "rock" || playerSelection.toLowerCase !== "paper" || playerSelection.toLowerCase !== "scissors"){
+        alert( "Invalid input")
+    }
+    else alert("Valid input")
+}
+
+console.log(RPS(1,2))

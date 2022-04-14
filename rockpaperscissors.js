9e78b2f5e798e7b3ce3584345e69a1d1811f933b
Philip <philiprkwan@gmail.com>
2022-04-13 19:57:19 -0600

    Add result variable

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -18,19 +18,23 @@ console.log(computerMove);
 return computerMove;
 }
 
-function RPS(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){     
+    let result = "";
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
         if (playerSelection == "rock"){
             switch (computerSelection){
                 case "rock":
                     alert("You chose rock, the Computer chose rock. You tie!");
+                    result = tie;
                     break;
                 case "paper":
                     alert("You chose rock, the Computer chose paper. You lose!");
+                    result = loss;
                     break;
                 case "scissors":
                     alert("You chose rock, the Computer chose scissors. You win!");
+                    result = win;
                     break;
             }
         }
@@ -38,12 +42,15 @@ function RPS(playerSelection,computerSelection){
             switch (computerSelection){
                 case "rock":
                     alert("You chose paper, the Computer chose rock. You win!");
+                    result = win;
                     break;
                 case "paper":
                     alert("You chose paper, the Computer chose paper. You tie!");
+                    result = tie;
                     break;
                 case "scissors":
                     alert("You chose paper, the Computer chose scissors. You lose!");
+                    result = loss;
                     break;
 
             }
@@ -52,12 +59,15 @@ function RPS(playerSelection,computerSelection){
             switch (computerSelection){
                 case "rock":
                     alert("You chose scissors, the Computer chose rock. You lose!");
+                    result = loss;
                     break;
                 case "paper":
                     alert("You chose scissors, the Computer chose paper. You win!");
+                    result = win;
                     break;
                 case "scissors":
                     alert("You chose scissors, the Computer chose scissors. You tie!");
+                    result = tie;
                     break;
             }
         }
@@ -66,5 +76,3 @@ function RPS(playerSelection,computerSelection){
     }
     }
 }
-
-RPS("RoCk",computerPlay())
